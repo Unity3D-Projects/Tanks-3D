8fdd21d6e7ab8b743c30bc03dfea073fa7f3e214
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-05-19 12:01:54 +0530

    Set up the tank behaviour

--- Tanks 3D/Assets/Scripts/Tank/TankMovement.cs
+++ Tanks 3D/Assets/Scripts/Tank/TankMovement.cs
@@ -2,78 +2,99 @@
 
 public class TankMovement : MonoBehaviour
 {
-    public int m_PlayerNumber = 1;         
-    public float m_Speed = 12f;            
-    public float m_TurnSpeed = 180f;       
-    public AudioSource m_MovementAudio;    
-    public AudioClip m_EngineIdling;       
-    public AudioClip m_EngineDriving;      
-    public float m_PitchRange = 0.2f;
+    public int playerNumber = 1;
+    public float speed = 12f;
+    public float turnSpeed = 180f;
+    public AudioSource movementAudio;
+    public AudioClip engineIdling;
+    public AudioClip engineDriving;
+    public float pitchRange = 0.2f;
 
-    /*
-    private string m_MovementAxisName;     
-    private string m_TurnAxisName;         
-    private Rigidbody m_Rigidbody;         
-    private float m_MovementInputValue;    
-    private float m_TurnInputValue;        
-    private float m_OriginalPitch;         
-
+    private string _movementAxis;
+    private string _turnAxis;
+    private Rigidbody _rb;
+    private float _movementInput;
+    private float _turnInput;
+    private float _originalPitch;
 
     private void Awake()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
     }
 
-
-    private void OnEnable ()
+    private void OnEnable()
     {
-        m_Rigidbody.isKinematic = false;
-        m_MovementInputValue = 0f;
-        m_TurnInputValue = 0f;
+        _rb.isKinematic = false;
+        _movementInput = 0f;
+        _turnInput = 0f;
     }
 
-
-    private void OnDisable ()
+    private void OnDisable()
     {
-        m_Rigidbody.isKinematic = true;
+        _rb.isKinematic = true;
     }
-
 
     private void Start()
     {
-        m_MovementAxisName = "Vertical" + m_PlayerNumber;
-        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+        _movementAxis = "Vertical" + playerNumber;
+        _turnAxis = "Horizontal" + playerNumber;
 
-        m_OriginalPitch = m_MovementAudio.pitch;
+        _originalPitch = movementAudio.pitch;
     }
-    */
 
     private void Update()
     {
         // Store the player's input and make sure the audio for the engine is playing.
-    }
+        _movementInput = Input.GetAxis(_movementAxis);
+        _turnInput = Input.GetAxis(_turnAxis);
 
+        EngineAudio();
+    }
 
     private void EngineAudio()
     {
         // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
+        if (Mathf.Abs(_movementInput) < 0.1f && Mathf.Abs(_turnInput) < 0.1f)
+        {
+            if (movementAudio.clip == engineDriving)
+            {
+                movementAudio.clip = engineIdling;
+                movementAudio.pitch = Random.Range(_originalPitch - pitchRange, _originalPitch + pitchRange);
+                movementAudio.Play();
+            }
+        }
+        else
+        {
+            if (movementAudio.clip == engineIdling)
+            {
+                movementAudio.clip = engineDriving;
+                movementAudio.pitch = Random.Range(_originalPitch - pitchRange, _originalPitch + pitchRange);
+                movementAudio.Play();
+            }
+        }
     }
-
 
     private void FixedUpdate()
     {
         // Move and turn the tank.
+        Move();
+        Turn();
     }
-
 
     private void Move()
     {
         // Adjust the position of the tank based on the player's input.
+        Vector3 movement = transform.forward * _movementInput * speed * Time.deltaTime;
+        _rb.MovePosition(_rb.position + movement);
     }
-
 
     private void Turn()
     {
         // Adjust the rotation of the tank based on the player's input.
+        float turn = _turnInput * turnSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        _rb.MoveRotation(_rb.rotation * turnRotation);
     }
 }
